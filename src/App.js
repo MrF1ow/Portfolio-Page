c6a2cd30e49fbf57a54e760f6a8e527761aa989b
Ethan Flow <109869245+MrF1ow@users.noreply.github.com>
2024-01-02 00:16:05 -0800

    Changed some things, need to add more

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Socials from './components/Socials';
+import Knowledge from './components/Knowledge';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div>
-        {/* <Navigation /> */}
         <Home />
+        <Knowledge />
         <Projects />
+        <Contact />
         <Socials />
 
     </div>
